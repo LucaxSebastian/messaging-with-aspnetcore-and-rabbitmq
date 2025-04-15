f1d2f051a62df2645591e3ab8404dc886382e61d
Lucas Sebastian <lucassebastian167@gmail.com>
2025-04-14 23:55:19 -0300

    feat: configure MassTransit with RabbitMQ

---
+++ domain/entities/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace messaging_with_aspnetcore_and_rabbitmq.domain.entities;
+
+public class Customer(int id, string fullName, string email)
+{
+    public int  Id { get; set; } = id;
+    public string FullName { get; set; } = fullName;
+    public string Email { get; set; } = email;
+}
