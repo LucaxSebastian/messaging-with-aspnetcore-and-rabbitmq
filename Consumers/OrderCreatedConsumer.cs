af0591775a63e208f4a7fdcf4ade11f724c0cc5d
Lucas Sebastian <lucassebastian167@gmail.com>
2025-04-14 23:51:54 -0300

    feat: create consumer

---
+++ Consumers/OrderCreatedConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using messaging_with_aspnetcore_and_rabbitmq.Contracts;
+
+namespace messaging_with_aspnetcore_and_rabbitmq.Consumers;
+
+public class OrderCreatedConsumer : IConsumer<OrderCreated>
+{
+    public OrderCreatedConsumer() { }
+
+    public Task Consume(ConsumeContext<OrderCreated> context)
+    {
+        Console.WriteLine(context.Message);
+
+        return  Task.CompletedTask; 
+    }
+}
+
