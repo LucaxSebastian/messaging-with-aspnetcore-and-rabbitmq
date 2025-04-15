a6e6d311a1cfbaa971fcec872a190cc25676ae56
Lucas Sebastian <lucassebastian167@gmail.com>
2025-04-14 23:53:34 -0300

    feat: create controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using messaging_with_aspnetcore_and_rabbitmq.Contracts;
+using messaging_with_aspnetcore_and_rabbitmq.domain.entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace messaging_with_aspnetcore_and_rabbitmq.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+public class OrdersController(IBus bus) : ControllerBase
+{
+    private readonly IBus _bus = bus;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] OrderCreated orderCreated)
+    {
+        var orderId = Guid.NewGuid();
+
+        var customer = new Customer(
+            orderCreated.Customer.Id,
+            orderCreated.Customer.FullName,
+            orderCreated.Customer.Email);
+
+        await _bus.Publish(new OrderCreated(orderId, customer));
+
+        return Ok(new { OrderId = orderId });
+    }
+}
+
