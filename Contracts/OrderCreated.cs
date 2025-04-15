254028e85468a15cea8f0481003270a2b3acfd4d
Lucas Sebastian <lucassebastian167@gmail.com>
2025-04-14 23:53:16 -0300

    feat: create event

---
+++ Contracts/OrderCreated.cs
@@ -0,0 +1,13 @@
+﻿using messaging_with_aspnetcore_and_rabbitmq.domain.entities;
+
+namespace messaging_with_aspnetcore_and_rabbitmq.Contracts;
+
+public class OrderCreated(Guid id, Customer customer)
+{
+    public Guid Id { get; set; } = id;
+    public Customer Customer { get; set; } = customer;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public override string ToString()
+        => $"Pedido criado! ID: {Id} para o cliente {Customer.FullName}. Data da criação: {CreatedAt:d}";
+}
